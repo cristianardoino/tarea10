41c6cd24bea6b90ef69e9709a036a38a1da5ec32
cristianardoino <davidardoino@gmail.com>
2023-05-21 16:19:10 -0300

    tarea10

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+let nombre = 'Juan'
+let apellido = 'Pepito'
+
+console.log(nombre + ' ' + apellido);
+
+let numero1 = 10;
+let numero2 = 20;
+let suma = numero1 + numero2;
+
+
+console.log('Suma:', suma)
+
+
+let numero3 = (30 + suma);
+
+console.log('numero3', numero3);
+
